28d0f3ba77906d5559ea7791b908e5d9cf817aa4
Ramiro Gaxiola <gax8627@gmail.com>
2026-02-18 05:16:50 -0800

    chore: Update branding (logo, favicon)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -73,7 +73,7 @@ export function Navbar() {
                     <Link href={`/${language}`} className="flex items-center group">
                         <div className="relative w-36 md:w-48 h-10 md:h-12 transition-all group-hover:scale-105 duration-300">
                             <Image
-                                src="/logo-new.png"
+                                src="/images/nearshore-logo-brand.png"
                                 alt="Nearshore Navigator Logo"
                                 fill
                                 className="object-contain dark:brightness-[1.2] dark:contrast-[1.1] dark:drop-shadow-[0_0_8px_rgba(255,255,255,0.1)]"
